84be143f01fadf9ff6ffa57c2eb87c1727dc8d28
Сергей_Я <tooniccee@gmail.com>
2020-03-02 00:24:03 +0800

    new project

---
+++ avia/precache-manifest.41ffaf53f7e6305610befff30619862c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feab85efc109b507cf84918d70485fc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed00c2f168c7524caa28",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bce282ba32ab752060e2",
+    "url": "./static/css/main.6b3cc973.chunk.css"
+  },
+  {
+    "revision": "ed00c2f168c7524caa28",
+    "url": "./static/js/2.d1d41983.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.d1d41983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce282ba32ab752060e2",
+    "url": "./static/js/main.19ac3235.chunk.js"
+  },
+  {
+    "revision": "c61401b78bc736e62801",
+    "url": "./static/js/runtime-main.91bb9685.js"
+  },
+  {
+    "revision": "96990a1b3638b68f706e0a3331be2821",
+    "url": "./static/media/Logo.96990a1b.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "./static/media/Shape.68ca57dc.svg"
+  }
+]);
