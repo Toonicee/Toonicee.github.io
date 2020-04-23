c51d65f15ef87fa86dd09a4f5ec8b6f2c845221a
Сергей_Я <tooniccee@gmail.com>
2020-04-23 16:03:16 +0800

    add

---
+++ precache-manifest.fc59a6430021b234eae13b657bc8c67d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6f6543da75b0e71548052f91348193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526367c91c6bd7787197",
+    "url": "/static/css/2.c6b8cacd.chunk.css"
+  },
+  {
+    "revision": "53e4670e7764b5de7b78",
+    "url": "/static/css/main.068c2d16.chunk.css"
+  },
+  {
+    "revision": "526367c91c6bd7787197",
+    "url": "/static/js/2.c910eb33.chunk.js"
+  },
+  {
+    "revision": "53e4670e7764b5de7b78",
+    "url": "/static/js/main.92bb334f.chunk.js"
+  },
+  {
+    "revision": "1e1efdcfffb4f20b6f85",
+    "url": "/static/js/runtime-main.3d35cc11.js"
+  },
+  {
+    "revision": "039ad4ce530adc756ada6408bb7e96d3",
+    "url": "/static/media/MusorC.039ad4ce.woff"
+  },
+  {
+    "revision": "b3681f129ff69936210f4301aec5a133",
+    "url": "/static/media/logo.b3681f12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.83ec02b6a53824aa46cb08f1a0a12f84.js"
+  "/precache-manifest.fc59a6430021b234eae13b657bc8c67d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
